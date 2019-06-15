a099cc5bffa921c3a72c27eb04b70bcf7fcc3a68
Aadil Sachdev <49340883+aadil-sachdev@users.noreply.github.com>
2019-06-15 19:08:14 +0530

    Update Array_Manupulation.cpp

--- Array_Manupulation.cpp
+++ Array_Manupulation.cpp
@@ -1,34 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-int fun(int n, int m, int arr[][3],int array[]);
+long fun(long n, long m, long arr[][3],long array[]);
 int main()
 {
-    int n,m;
+    long n,m;
     cin>>n>>m;
-    int array[n]={0};
-    int arr[m][3];
-    for(int i=0;i<m;i++)
+    long array[n]={0};
+    long arr[m][3];
+    for(long i=0;i<m;i++)
     {
-        for(int j=0;j<3;j++)
+        for(long j=0;j<3;j++)
         {
             cin>>arr[i][j];
         }
     }
-    int ans=fun(n,m,arr,array);
+    long ans=fun(n,m,arr,array);
     cout<<ans;
 }
 
-int fun(int n,int m,int arr[][3],int array[])
+long fun(long n,long m,long arr[][3],long array[])
 {
-    for(int i=0;i<m;i++)
+    for(long i=0;i<m;i++)
     {
-        for(int j=arr[i][0];j++;j<arr[i][1])
+        for(long j=arr[i][0]-1;j<=arr[i][1]-1;j++)
         {
             array[j]+=arr[i][2];
         }
+        
     }
-    int max=0;
-    for(int i=0;i<n;i++)
+    long max=0;
+    for(long i=0;i<n;i++)
     {
         if(array[i]>max)
         max=array[i];
