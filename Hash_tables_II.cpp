f78b4d0a493cbf92011cbc6081ae4e7a148caa20
Aadil Sachdev <49340883+aadil-sachdev@users.noreply.github.com>
2019-06-19 18:40:30 +0530

    Create Hash_tables_II.cpp

---
+++ Hash_tables_II.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m,n;
+    cin>>m>>n;
+    map<string,int> mag;
+    map<string,int> line;
+    string word;
+    for(int i=0;i<m;i++)
+    {
+        cin>>word;
+        mag[word]++;
+    }    
+    for(int i=0;i<n;i++)
+    {
+        cin>>word;
+        line[word]++;
+    }
+    int success=0;
+    map<string,int>::iterator it;
+    for(it=line.begin();it!=line.end();it++)
+    {
+        if(it->second>mag[it->first])
+        success++;
+        break;
+    }
+    if(success)
+    cout<<"No";
+    else
+    cout<<"Yes";
+}
