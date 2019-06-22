c682f499f15875365d4ca20a5ac181d077ab27ef
Aadil Sachdev <49340883+aadil-sachdev@users.noreply.github.com>
2019-06-22 14:59:54 +0530

    Create Two_Strings.cpp

---
+++ Two_Strings.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int strr(string, string);
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+    string s1, s2;
+    cin>>s1>>s2;
+    int ans=strr(s1,s2);
+    if(ans)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+    }
+    
+}
+
+int strr(string s1, string s2)
+{
+    std::string::iterator it;
+    map<char,int> iden1;
+    map<char,int> iden2;
+    for(it=s1.begin();it!=s1.end();it++)
+    {
+        iden1[*it]++;
+    }
+    for(it=s2.begin();it!=s2.end();it++)
+    {
+        iden2[*it]++;
+    }
+    map<char,int> :: iterator ite;
+    for(ite=iden1.begin();ite!=iden1.end();ite++)
+    {
+        if(iden1[ite->first]&&iden2[ite->first])
+        {
+            return 1;
+        }
+    }
+    return 0;
+
+}
