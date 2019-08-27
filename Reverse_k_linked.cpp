227400b89735ace94b7715216433d4f4f9ffefbe
Aadil Sachdev <49340883+aadil-sachdev@users.noreply.github.com>
2019-08-27 14:41:44 +0530

    Update Reverse_k_linked.cpp

--- Reverse_k_linked.cpp
+++ Reverse_k_linked.cpp
@@ -1,11 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+/* Link list node */
+struct node *reverse (struct node *head, int k);
+struct node
+{
+    int data;
+    struct node* next;
+}*head;
+/* UTILITY FUNCTIONS */
+/* Function to push a node */
+  void insert()
+{
+    int n,value;
+    cin>>n;
+    struct node *temp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            head=(struct node *) malloc( sizeof(struct node) );
+            head->data=value;
+            head->next=NULL;
+            temp=head;
+            continue;
+        }
+        else
+        {
+            temp->next= (struct node *) malloc( sizeof(struct node) );
+            temp=temp->next;
+            temp->data=value;
+            temp->next=NULL;
+        }
+    }
+}
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+cout<<" ";
+}
+/* Drier program to test above function*/
+int main(void)
+{
+    /* Start with the empty list */
+    int t,k,value,n;
+     cin>>t;
+     while(t--)
+     {
+     insert();
+     cin>>k;
+     head = reverse(head, k);
+     printList(head);
+     }
+     return(0);
+}
 
-  node *ptr=head;
-  if(ptr->next==NULL)
-  return ptr;
-  node*cur=ptr;
-  node*prev=NULL;
-  node*nex= NULL;
-  while(k--)
+
+/*This is a function problem.You only need to complete the function given below*/
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+  struct node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+
+
+struct node *reverse (struct node *head, int k)
+{
+    node *prev,*ptr,*cur,*nex;
+    cur=head;
+    ptr=head;
+    while(k--&&cur!=NULL)
     {
         nex=cur->next;
         cur->next=prev;
@@ -14,18 +89,23 @@
     }
     head=prev;
     ptr->next=cur;
-    ptr=prev;
-    while(nex!=NULL)
+    prev=ptr;
+    while(cur!=NULL)
     {
-        while(k--&&nex!=NULL)
+        while(k--&&cur!=NULL)
         {
-        nex=cur->next;
-        cur->next=prev;
-        prev=cur;
-        cur=nex;   
+            nex=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=nex;
         }
-    ptr->next->next=cur;
-    ptr->next=prev;
-    ptr=prev;
+        ptr->next=prev;
+        ptr->next->next=cur;
+        while(prev->next!=cur)
+        {
+            prev=prev->next;
+        }
+        ptr=prev;
     }
     return head;
+}
